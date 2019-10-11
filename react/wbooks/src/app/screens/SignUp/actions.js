9a8220cc78a17bb5ed76fdb090aa17dc3a18041f
guillewolox <guillermo.sequeira@wolox.com.ar>
2019-10-11 11:44:46 -0300

    delete comment

--- react/wbooks/src/app/screens/SignUp/actions.js
+++ react/wbooks/src/app/screens/SignUp/actions.js
@@ -9,16 +9,3 @@ export const actionCreateUser = async user => {
   }
   return { ok: false, problem: userCreated.problem };
 };
-
-// export const actionCreateUser = user =>
-//   // eslint-disable-next-line no-async-promise-executor
-//   new Promise(async resolve => {
-//     // const tokens = await socialAuthApi.post('/oauth2/token', body);
-//     const userNormalize = normalizeCreateUser(user);
-//     const userCreated = await createUser(userNormalize);
-//     if (userCreated.ok) {
-//       resolve({ ok: true, data: userCreated.data });
-//     } else {
-//       resolve({ ok: false, problem: userCreated.problem });
-//     }
-//   });
