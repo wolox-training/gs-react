53f02c268c3471bd9825fd9078a89285c2f3f770
guillewolox <guillermo.sequeira@wolox.com.ar>
2019-10-16 11:01:44 -0300

    change name of paths

--- react/wbooks/src/app/index.js
+++ react/wbooks/src/app/index.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <Router>
       <div className={styles.container}>
-        <Route exact path="/Signup" component={SignUp} />
-        <Route path="/Login" component={Login} />
+        <Route exact path="/sign_up" component={SignUp} />
+        <Route path="/" component={Login} />
       </div>
     </Router>
   );
